adc0ea1b89ceb4fd7bba427b842ec463e471e66f
sergihg <sandreshdz@gmail.com>
2023-01-29 13:40:29 -0600

    cambio en el enhanced switch

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,12 @@ public class Main {
             int parameter1 = Integer.parseInt(args[operationIndex + 1]);
             int parameter2 = Integer.parseInt(args[operationIndex + 2]);
             switch (operation) {
-                case "SUMA":
-                    System.out.println(parameter1 + " + " + parameter2 + " = " + (parameter1 + parameter2));
-                    break;
-                case "RESTA":
-                    System.out.println(parameter1 + " - " + parameter2 + " = " + (parameter1 - parameter2));
-                    break;
-                case "MULTIPLICACION":
-                    System.out.println(parameter1 + " * " + parameter2 + " = " + (parameter1 * parameter2));
-                    break;
-                case "DIVISION":
-                    System.out.println(parameter1 + " / " + parameter2 + " = " + (parameter1 / parameter2));
-                    break;
+                case "SUMA" -> System.out.println(parameter1 + " + " + parameter2 + " = " + (parameter1 + parameter2));
+                case "RESTA" -> System.out.println(parameter1 + " - " + parameter2 + " = " + (parameter1 - parameter2));
+                case "MULTIPLICACION" ->
+                        System.out.println(parameter1 + " * " + parameter2 + " = " + (parameter1 * parameter2));
+                case "DIVISION" ->
+                        System.out.println(parameter1 + " / " + parameter2 + " = " + (parameter1 / parameter2));
             }
             operationIndex+=3;
         }while(operationIndex< args.length);
